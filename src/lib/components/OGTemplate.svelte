d4ffbf001cfef53ac580a3dc0c532d8169fe9189
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2023-11-04 01:37:22 +0530

    parameterised og image maker

--- src/lib/components/OGTemplate.svelte
+++ src/lib/components/OGTemplate.svelte
@@ -2,6 +2,8 @@
 	// can use to design: https://og-playground.vercel.app/
 	export let text = 'SudoMakes';
 	export let subtext = 'SudoMakes';
+	export let bgColor = '';
+	export let textColor = '';
 	export let logo = 'https://sudomakes.art/logo.png';
 	import { config, openGraph } from '$lib/config';
 	const height = openGraph.height;
@@ -12,10 +14,12 @@
 <div style="display:flex;height:100%;width:100%;background:white">
 	<div style="left:0;top:0;bottom:0;width:{barWidth}px;position:absolute;display:flex;">
 		<div
-			style="display:flex;background:#064e3b;width:{height}px;justify-content:center;transform-origin:top left;transform:rotate(-90deg) translateX(-100%);border-bottom: 8px solid {color};padding:20px;"
+			style="display:flex;background:{bgColor};width:{height}px;justify-content:center;transform-origin:top left;transform:rotate(-90deg) translateX(-100%);border-bottom: 8px solid {textColor};padding:20px;"
 		>
 			<img src={logo} style="width:100px; height:100px;" alt="logo" />
-			<div style="font-size:80px;color:{color};font-weight:800;line-height:85%;margin-left:20px;">
+			<div
+				style="font-size:80px;color:{textColor};font-weight:800;line-height:85%;margin-left:20px;"
+			>
 				{subtext}
 			</div>
 		</div>
--- src/routes/og/+server.js
+++ src/routes/og/+server.js
@@ -1,22 +1,29 @@
 // used the following as a reference: https://geoffrich.net/posts/svelte-social-image/
-import satori from 'satori';
-import { Resvg } from '@resvg/resvg-js';
-import Saira from '$lib/fonts/Saira-ExtraBold.ttf';
-import OpenSans from '$lib/fonts/OpenSans-ExtraBold.ttf';
-import { html as toReactNode } from 'satori-html';
 import OGTemplate from '$lib/components/OGTemplate.svelte';
-import { openGraph } from '$lib/config';
-
-const width = openGraph.width;
-const height = openGraph.height;
+import OpenSans from '$lib/fonts/OpenSans-ExtraBold.ttf';
+import Saira from '$lib/fonts/Saira-ExtraBold.ttf';
+import { Resvg } from '@resvg/resvg-js';
+import * as Vibrant from 'node-vibrant';
+import satori from 'satori';
+import { html as toReactNode } from 'satori-html';
+// const width = openGraph.width;
+// const height = openGraph.height;
 
 /** @type {import('./$types').RequestHandler} */
 export const GET = async ({ url }) => {
 	const text = url.searchParams.get('text') ?? 'SudoMakes';
 	const subtext = url.searchParams.get('subtext') ?? 'SudoMakes';
 	const logo = url.searchParams.get('logo') ?? 'https://sudomakes.art/logo.png';
-	const result = OGTemplate.render({ text, subtext, logo });
+	const colorProps = await Vibrant.from(logo)
+		.getPalette()
+		.then((p) => ({
+			bgColor: url.searchParams.get('bgColor') || p.DarkVibrant.hex,
+			textColor: url.searchParams.get('textColor') || p.LightVibrant.hex
+		}));
+	// console.log(light, 'light color');
+	const result = OGTemplate.render({ text, subtext, logo, ...colorProps });
 	const element = toReactNode(result.html);
+	const mode = url.searchParams.get('mode') ?? 'og';
 	// const element = toReactNode(`${result.html}<style>${result.css.code}</style>`);
 
 	const svg = await satori(element, {
@@ -24,14 +31,14 @@ export const GET = async ({ url }) => {
 			{ name: 'Saira', data: Buffer.from(Saira), style: 'bold' },
 			{ name: 'Open Sans', data: Buffer.from(OpenSans), style: 'bold' }
 		],
-		height,
-		width
+		height: mode === 'twitter' ? 600 : 630,
+		width: 1200
 	});
 
 	const resvg = new Resvg(svg, {
 		fitTo: {
 			mode: 'width',
-			value: width
+			value: 1200
 		}
 	});
 
